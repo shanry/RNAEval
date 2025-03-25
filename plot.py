66e12bba4242767613c8ba5fd0c55b6d5511c60c
Tianshou Zhou <zhoutian@oregonstate.edu>
2025-03-25 01:03:47 -0700

    adjust fonts and margins

--- plot.py
+++ plot.py
@@ -6,6 +6,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
+FONT_SIZE_1 = 24
+FONT_SIZE_2 = 18
+
+
 def plot(file, outfile='entropy_ave'):
     df = pd.read_csv(file)
     # filter rows whose name contains Ours
@@ -17,21 +21,36 @@ def plot(file, outfile='entropy_ave'):
 
     # plot the data
     _, ax = plt.subplots()
-    ax.scatter(x, y, c="blue", alpha=0.5)
-    plt.xlabel("LinearDesign")
-    plt.ylabel("Ours")
+    # fig, ax = plt.subplots(figsize=(8, 8))
+    ax.scatter(x, y, c="blue", alpha=0.7)
+    plt.xlabel("LinearDesign", fontsize=FONT_SIZE_1)
+    plt.ylabel("Ours", fontsize=FONT_SIZE_1)
+
+    # set fontsize of x and y ticks
+    plt.xticks(fontsize=FONT_SIZE_2)
+    plt.yticks(fontsize=FONT_SIZE_2)
 
     # make the scale of x and y the same
     ax.set_aspect("equal")
+
+    # tighten layout, remove padding
+    plt.tight_layout(pad=0.1)
 
     # draw a diagonal line
     min_val = min(x.min(), y.min())
     max_val = max(x.max(), y.max())
     ax.plot([min_val, max_val], [min_val, max_val], color="red")
 
+    # Remove excess whitespace with these settings
+    # plt.tight_layout(pad=0.0)
+    # Adjust the subplot parameters to eliminate margins
+    # plt.subplots_adjust(left=0.1, right=0.95, top=0.95, bottom=0.1)
+
+
     # save the plot to a pdf file
-    plt.savefig(f"plots/{outfile}.pdf")
-    plt.show()
+    plt.savefig(f"plots/{outfile}.pdf", bbox_inches='tight', pad_inches=0.05)
+    print(f"file saved to plots/{outfile}.pdf")
+    # plt.show()
 
 
 if __name__ == "__main__":
